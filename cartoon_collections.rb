5e4e5a3701b296f1736128900ae3121c4fffd06e
samumar43 <samumar43@gmail.com>
2020-11-22 22:53:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,9 @@ def greet_characters(array)
   # Print a custom greeting for each element
   index = 0
   array.each do |element|
+    puts "Hello #{element}"
+  end
+  index += 1
 end
 
 def list_dwarves(array)
